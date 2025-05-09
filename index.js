3a45610a70cb99c6fdf5ba52aa5f306e3f40c3c5
nuryme <hello.nurnahar@gmail.com>
2025-05-09 21:55:47 +0600

    done with the server side

--- index.js
+++ index.js
@@ -65,6 +65,22 @@ async function run() {
 
     const bioCollection = matrimony.collection('bio-data')
     const paymentCollection = matrimony.collection('payment')
+    const favoriteCollection = matrimony.collection('favorite')
+    const userCollection = matrimony.collection('user')
+    const marriedCollection = matrimony.collection('married')
+
+
+    const verifyAdmin = async (req, res, next ) => {
+      const email = req.user?.email
+      const query={email}
+      const result = await userCollection.findOne(query)
+      if(!result || result.role !== 'admin') {
+        return res.status(403).send({message: 'Forbidden access. Admin only actions!'})
+      }
+
+      next()
+    }
+    
 
 
     app.post('/jwt', async (req, res) => {
@@ -97,6 +113,165 @@ async function run() {
       catch (err) {
         res.status(500).send(err)
       }
+    }
+    )
+    
+
+
+    // users collection
+    app.post('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const query = {email}
+      const user = req.body
+      const isExist = await userCollection.findOne(query)
+      if(isExist) {
+       return res.send({message: 'User already exist'})
+      }
+      const result = await userCollection.insertOne({...user, role: 'user', timeStamp: Date.now()})
+      res.send(result)
+    }
+    )
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      const email = req.user?.email
+      const username = req.query.username
+      console.log(username)
+      let query= {email: {$ne: email}, isPremium: {$in: ['request', 'premium']} }
+
+      if(username && username.trim() !== '') {
+        query.name = { $regex: username.trim(), $options: 'i' };
+      }
+
+      const result = await userCollection.find(query).toArray()
+      res.send(result)
+    }
+    )
+    // premium approval requests
+    app.get('/users/premium-request', verifyToken, verifyAdmin, async (req, res) => {
+      const email = req.user?.email
+      const query= {email: {$ne: email}, isPremium: 'request' }
+      const result = await userCollection.find(query).toArray()
+      res.send(result)
+    }
+    )
+    app.get('/premium-members', async (req, res) => {
+      const query= {isPremium: 'premium' }
+
+      const result = await userCollection.aggregate([
+        {$match: query},
+        {
+          $lookup: {
+            from: 'bio-data',
+            localField: 'email',
+            foreignField: 'email',
+            as: 'bio_data'
+          }
+        },
+        {
+          $unwind: '$bio_data'
+        }
+      ]).toArray()
+      res.send(result)
+    }
+    )
+    app.get('/user/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      const query= {email}
+      const result = await userCollection.findOne(query)
+      res.send(result)
+    }
+    )
+    // get a user's role 
+    app.get('/user/role/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      const query = {email}
+      const result = await userCollection.findOne(query)
+      res.send({role: result.role})
+    }
+    )
+    // premium request
+    app.patch('/make-premium/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      const isAdmin= req.query.isAdmin
+      const isPremium= req.query.isPremium
+      const bioId = req.query.bioId
+      const query= {email}
+      const user= await userCollection.findOne(query)
+      let updatedDoc = {}
+      
+      if(isPremium && isPremium === 'request') {
+         updatedDoc = {$set: {...user, isPremium: 'request', premiumId: parseInt(bioId)}}
+
+      }
+
+      if(isPremium && isPremium === 'premium') {
+         updatedDoc = {$set: {...user, isPremium: 'premium'}}
+
+      }
+
+      if(isAdmin && isAdmin === 'admin') {
+         updatedDoc = {$set: {...user, role: 'admin'}}
+
+      }
+      const result = await userCollection.updateOne(query, updatedDoc)
+      res.send(result)
+    }
+    )
+    // make admin
+    
+
+
+
+
+    // admin dashboard
+    app.get('/admin-dashboard', verifyToken, verifyAdmin, async (req, res) => {
+      const bio = await bioCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalBio: {$sum: 1},
+            maleBio: {$sum: {
+              $cond: [ {$eq: ['$type',  'Male']}, 1, 0]
+            }},
+            femaleBio: {
+              $sum: {
+                $cond: [{$eq: ['$type', 'Female']}, 1, 0]
+              }
+            },
+          }
+        },
+        {
+          $project: {
+            _id: 0
+          }
+        }
+      ]).next()
+
+      const payment = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalRevenue: {$sum: '$amount'}
+          }
+        },
+        {$project: {_id: 0}}
+      ]).next()
+
+
+      const premium = await userCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            premiumBio: {
+              $sum: {
+                $cond: [{$eq: ['$isPremium', 'premium']}, 1, 0]
+              }
+            }
+          }
+        },
+        {$project: {_id: 0}}
+      ]).next()
+
+      res.send({...bio, ...payment, ...premium})
     }
     )
 
@@ -164,6 +339,41 @@ async function run() {
     )
 
 
+    // success counter
+    app.get('/success-counter', async (req, res) => {
+      const bio = await bioCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalBio: {$sum: 1},
+            maleBio: {$sum: {
+              $cond: [ {$eq: ['$type',  'Male']}, 1, 0]
+            }},
+            femaleBio: {
+              $sum: {
+                $cond: [{$eq: ['$type', 'Female']}, 1, 0]
+              }
+            },
+            // marriage: {
+            //   $sum: {
+            //     $cond: [{$eq: ['$isPremium', 'premium']}, 1, 0]
+            //   }
+            // }
+          }
+        },
+        {
+          $project: {
+            _id: 0
+          }
+        }
+      ]).next()
+
+      res.send(bio)
+    }
+    )
+
+
+
     // stripe collection
     app.post('/create-payment-intent', verifyToken, async (req, res) => {
       const { money } = req.body
@@ -187,6 +397,26 @@ async function run() {
       res.send(result)
     }
     )
+    // approval contact request
+    app.get('/approval-contact-request', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await paymentCollection.find({status: 'pending'}).toArray()
+      res.send(result)
+    }
+    )
+    app.patch('/approval-contact-request/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id
+
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          status: 'approved'
+        }
+      }
+      const result = await paymentCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    }
+    )
+
 
 
     // my contact request collection
@@ -232,9 +462,52 @@ async function run() {
 
     // favorite collection
     app.post('/favorite', verifyToken, async (req, res) => {
-      const favoriteUser = 
+      const favoriteUser = req.body
+      const result = await favoriteCollection.insertOne(favoriteUser)
+      res.send(result)
     }
     )
+    app.get('/favorite/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      const query = {email}
+      const result = await favoriteCollection.find(query).toArray()
+      res.send(result)
+    }
+    )
+    app.delete('/favorite/:id', verifyToken, async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await favoriteCollection.deleteOne(query)
+      res.send(result)
+    }
+    )
+
+
+    //married collection
+    app.post('/married', verifyToken, async (req, res) => {
+      const info = req.body
+      const result = await marriedCollection.insertOne(info)
+      res.send(result)
+    }
+    )
+    app.get('/married', async (req, res) => {
+      const result = await marriedCollection.aggregate([
+        {$sort: {_id: -1}},
+        {
+          $addFields: {
+            marriage_date: {
+              $dateToString: {
+                format: '%Y-%m-%d',
+                date: { $toDate: '$_id' }
+              }
+            }
+          }
+        }
+      ]).toArray()
+      res.send(result)
+    }
+    )
+
 
 
     // Connect the client to the server	(optional starting in v4.7)
